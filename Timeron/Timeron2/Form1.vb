68c7b722ad17ebc6ce5eb32a4ba69938d916e634
김정혁 <tkcjsrn@naver.com>
2021-04-21 01:26:37 +0900

    Create Form1.vb

---
+++ Timeron/Timeron2/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmTimer
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 1000
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub TimerEnabled_Click(sender As Object, e As EventArgs) Handles cmdStart.Click
+        If cmdStart.Text = "Stop" Then
+            cmdStart.Text = "Start"
+            Timer1.Enabled = False
+        Else
+            cmdStart.Text = "Stop"
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If picTimer.BackColor = Color.Red Then
+            picTimer.BackColor = Color.Blue
+        Else
+            picTimer.BackColor = Color.Red
+        End If
+
+    End Sub
+
+    Private Sub picTimer_Click(sender As Object, e As EventArgs) Handles picTimer.Click
+
+    End Sub
+End Class
