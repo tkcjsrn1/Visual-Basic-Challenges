3428e0d25d8ddedd9058067a5ff30b69cf717518
김정혁 <tkcjsrn@naver.com>
2021-04-21 02:29:21 +0900

    Create Form1.vb

---
+++ NumericUpDown/NumericUpDown/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmPicColor
+
+    Private Sub NumericRed_Changed(sender As Object, e As EventArgs) Handles NumericRed.ValueChanged
+        picColor.BackColor = Color.FromArgb(255, NumericRed.Value, NumericGreen.Value, NumericBlue.Value)
+    End Sub
+    Private Sub NumericGreen_Changed(sender As Object, e As EventArgs) Handles NumericGreen.ValueChanged
+        picColor.BackColor = Color.FromArgb(255, NumericRed.Value, NumericGreen.Value, NumericBlue.Value)
+    End Sub
+
+    Private Sub NumericBlue_Changed(sender As Object, e As EventArgs) Handles NumericBlue.ValueChanged
+        picColor.BackColor = Color.FromArgb(255, NumericRed.Value, NumericGreen.Value, NumericBlue.Value)
+    End Sub
+End Class
