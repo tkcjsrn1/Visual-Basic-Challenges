7c38ebec2d20fe51b72975229a9790e734fa4d20
김정혁 <tkcjsrn@naver.com>
2021-05-05 05:28:38 +0900

    Create Form1.vb

---
+++ variableTest/variableTest/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmDataType
+    Private Sub sum_Click(sender As Object, e As EventArgs) Handles cmdSum.Click
+        Dim Kor, Mat, Eng, Total As Integer
+        Kor = Val(txtKor.Text)
+        Mat = Val(txtMath.Text)
+        Eng = Val(txtEng.Text)
+        Total = Kor + Mat + Eng
+        lblOutput.Text = Str(Total)
+    End Sub
+End Class
