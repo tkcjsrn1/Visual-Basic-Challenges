137310fcec82168623e7e81b25a621e2ab058c48
김정혁 <tkcjsrn@naver.com>
2021-05-12 01:15:01 +0900

    Create Form1.vb

---
+++ ifThen/ifThen/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class frmIfThen
+    Private Sub lblGrade_Click(sender As Object, e As EventArgs) Handles lblGrade.Click
+
+    End Sub
+
+    Private Sub cmdCaculate_Click(sender As Object, e As EventArgs) Handles cmdCaculate.Click
+        Dim Kor, Eng, Mat, Total As Double
+        Dim Aver, Average As Double
+        Kor = Val(txtKor.Text)
+        Eng = Val(txtEng.Text)
+        Mat = Val(txtMath.Text)
+        Total = Kor + Eng + Mat
+        Aver = Total / 3
+        Average = Math.Round(Aver, 2)
+        If Average >= 90 Then
+            lblGradeDisp.Text = "A"
+        ElseIf Average >= 80 Then
+            lblGradeDisp.Text = "B"
+        ElseIf Average >= 70 Then
+            lblGradeDisp.Text = "C"
+        Else
+            lblGradeDisp.Text = "F"
+        End If
+
+    End Sub
+End Class
